503d57901b9b41108239a1f65d7cf9973c6642a1
刘猛 <1156858877@qq.com>
2024-06-22 17:17:39 +0800

    移除报错代码.

--- test/ff_sku_picker_test.dart
+++ test/ff_sku_picker_test.dart
@@ -1,10 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockFfSkuPickerPlatform with MockPlatformInterfaceMixin {
-
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
+// class MockFfSkuPickerPlatform with MockPlatformInterfaceMixin {
+//   Future<String?> getPlatformVersion() => Future.value('42');
+// }
 
 void main() {
 
